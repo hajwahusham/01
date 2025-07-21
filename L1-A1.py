e374b1b00c9e516c19b56cedefa97c7a7a07ded6
hajwahusham <hajwahusham@gmail.com>
2025-07-21 17:33:06 +0500

    activity 01

---
+++ L1-A1.py
@@ -0,0 +1,18 @@
+# Printing a greeting message
+print("Hello, World")
+
+# Printing my name
+print("my name is hajwa")
+
+# Printing a fun fact about myself
+print("i love reading books")
+
+# Performing and printing a simple calculation
+print("the result of 2 + 5 is: ", 2 + 5)
+
+'''
+this 
+is 
+multiple line
+comments!
+'''
