52e17b36d8be00947d1bda463ad06f4e61749782
hajwahusham <hajwahusham@gmail.com>
2025-07-21 17:44:03 +0500

    activity 02

---
+++ L1-A4.py
@@ -0,0 +1,12 @@
+# Printing a sentence
+print("Welcome to the wolrd of programming")
+
+# Print a number
+print(8)
+
+# Next line
+print("hello world \n \n this sentence is coming in a new line")
+
+# Print all keywords
+import keyword
+print(keyword.kwlist)
